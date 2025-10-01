aefda03591b405ca831f75831867a86f44ce22b8
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-01 15:36:35 +0700

    TH_Quizzapp_Total

--- TH_Quizzapp_Total/app/src/main/java/th/nguyenhuutrong/th_quizzapp_total/MainActivity.java
+++ TH_Quizzapp_Total/app/src/main/java/th/nguyenhuutrong/th_quizzapp_total/MainActivity.java
@@ -3,6 +3,7 @@ package th.nguyenhuutrong.th_quizzapp_total;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,7 +16,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txtCauHoi;
+    TextView txtCauHoi, txtScore;
     Button[] buttons = new Button[4];
     CauHoi cauHoiHienTai;
     int score = 0;
@@ -48,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         txtCauHoi.setText(cauHoiHienTai.getCauHoiText());
 
         for (int i = 0; i < 4; i++) {
+            buttons[i].setEnabled(true);
             buttons[i].setText(String.valueOf(cauHoiHienTai.dapAnList.get(i)));
             buttons[i].setBackgroundResource(R.drawable.btn_bg);
             int finalI = i;
@@ -59,7 +61,33 @@ public class MainActivity extends AppCompatActivity {
                         score++;
                         capNhatScore();
                         buttons[finalI].setBackgroundColor(Color.parseColor("#4CAF50"));
-                        buttons[finalI].postDelayed(() -> loadCauHoiMoi(), 1000);
+
+                        for (Button b : buttons) {
+                            b.setEnabled(false);
+                        }
+
+                        AlphaAnimation fadeOut = new AlphaAnimation(1.0f, 0.0f);
+                        fadeOut.setDuration(500);
+                        fadeOut.setFillAfter(true);
+
+                        txtCauHoi.startAnimation(fadeOut);
+                        for (Button b : buttons) {
+                            b.startAnimation(fadeOut);
+                        }
+
+                        buttons[finalI].postDelayed(() -> {
+                            loadCauHoiMoi();
+
+                            AlphaAnimation fadeIn = new AlphaAnimation(0.0f, 1.0f);
+                            fadeIn.setDuration(500);
+                            fadeIn.setFillAfter(true);
+
+                            txtCauHoi.startAnimation(fadeIn);
+                            for (Button b : buttons) {
+                                b.startAnimation(fadeIn);
+                            }
+
+                        }, 1000);
                     } else {
                         score = 0;
                         capNhatScore();

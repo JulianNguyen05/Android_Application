e79182ea070cc6f7dd3a0ec0f05303aa0425d84d
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-03 21:53:03 +0700

    home screen

--- lamthemTicTacToe/app/src/main/java/th/nguyenhuutrong/lamthemtictactoe/MainActivity.java
+++ lamthemTicTacToe/app/src/main/java/th/nguyenhuutrong/lamthemtictactoe/MainActivity.java
@@ -8,7 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    private final List<int[]> combinationList = new ArrayList<>();
+
+    private int[] boxPositions = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private int playerTurn = 1;
+    private int totalSelectedBoxes = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void restartMatch() {
+        boxPositions = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+        playerTurn = 1;
+        totalSelectedBoxes = 1;
+
+        binding.image1.setImageResource(R.drawable.white_box);
+        binding.image2.setImageResource(R.drawable.white_box);
+        binding.image3.setImageResource(R.drawable.white_box);
+        binding.image4.setImageResource(R.drawable.white_box);
+        binding.image5.setImageResource(R.drawable.white_box);
+        binding.image6.setImageResource(R.drawable.white_box);
+        binding.image7.setImageResource(R.drawable.white_box);
+        binding.image8.setImageResource(R.drawable.white_box);
+        binding.image9.setImageResource(R.drawable.white_box);
     }
 }

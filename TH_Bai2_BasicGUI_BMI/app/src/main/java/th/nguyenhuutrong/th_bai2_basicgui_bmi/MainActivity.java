723a79e323b99f594ebc907872347924e7cc2113
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-07 23:05:39 +0700

    add Main

--- TH_Bai2_BasicGUI_BMI/app/src/main/java/th/nguyenhuutrong/th_bai2_basicgui_bmi/MainActivity.java
+++ TH_Bai2_BasicGUI_BMI/app/src/main/java/th/nguyenhuutrong/th_bai2_basicgui_bmi/MainActivity.java
@@ -1,5 +1,7 @@
 package th.nguyenhuutrong.th_bai2_basicgui_bmi;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +12,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -38,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         btnCaculate = findViewById(R.id.btnCalculate);
         tvResult = findViewById(R.id.tvResult);
 
+        btnCaculate.setOnClickListener(v -> CaculateBMI());
     }
 
     public void CaculateBMI(){
@@ -62,9 +66,11 @@ public class MainActivity extends AppCompatActivity {
             boolean isAsian = switchAsia.isChecked();
 
             String resultCategory = classifyBMI(bmi, isAsian);
+            int color = getBMIColor(bmi, isAsian);
 
-            String resultText = String.format("BMI: %1.f\n%s", bmi, resultCategory);
+            String resultText = String.format("BMI: %.1f\n%s", bmi, resultCategory);
             tvResult.setText(resultText);
+            tvResult.setTextColor(color);
 
         } catch (NumberFormatException e){
             Toast.makeText(this, "Dữ liệu nhập không hợp lệ!", Toast.LENGTH_SHORT).show();
@@ -82,6 +88,22 @@ public class MainActivity extends AppCompatActivity {
             else if(bmi < 25) return "Bình thường";
             else if(bmi < 30) return "Thừa cân";
             else return "Béo phì";
+        }
+    }
+
+    private int getBMIColor(double bmi, boolean isAsian){
+        double underWeight = isAsian ? 17.5 : 18.5;
+        double normalWeight = isAsian ? 23 : 25;
+        double overWeight = isAsian ? 28 : 30;
+
+        if(bmi < underWeight){
+            return ContextCompat.getColor(this, R.color.under_weight);
+        }else if(bmi < normalWeight){
+            return ContextCompat.getColor(this, R.color.normal_weight);
+        }else if(bmi < overWeight){
+            return ContextCompat.getColor(this, R.color.over_weight);
+        }else{
+            return ContextCompat.getColor(this, R.color.obese);
         }
     }
 

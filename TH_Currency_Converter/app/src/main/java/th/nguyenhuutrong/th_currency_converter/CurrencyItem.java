82295bcae83a13023c674408f274f52bead93fdb
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-08 15:35:32 +0700

    first commit

---
+++ TH_Currency_Converter/app/src/main/java/th/nguyenhuutrong/th_currency_converter/CurrencyItem.java
@@ -0,0 +1,19 @@
+package th.nguyenhuutrong.th_currency_converter;
+
+public class CurrencyItem {
+    private final String code;
+    private final int flagResId;
+
+    public CurrencyItem(String code, int flagResId) {
+        this.code = code;
+        this.flagResId = flagResId;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    public int getFlagResId() {
+        return flagResId;
+    }
+}

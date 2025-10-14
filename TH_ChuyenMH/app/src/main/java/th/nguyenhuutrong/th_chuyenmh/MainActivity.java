17a2d501e4e519d21e5526792a7b21f695cc4cb6
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-14 22:34:55 +0700

    B1, gửi từ MH1 sang MH2

--- TH_ChuyenMH/app/src/main/java/th/nguyenhuutrong/th_chuyenmh/MainActivity.java
+++ TH_ChuyenMH/app/src/main/java/th/nguyenhuutrong/th_chuyenmh/MainActivity.java
@@ -1,6 +1,8 @@
 package th.nguyenhuutrong.th_chuyenmh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void ChuyenSangMH2(View v){
+        // 1. Tạo đối tượng Intent
+        Intent thuKichHoatMH2;
+
+            // Hàm tạo có 2 tham số, tham số 2 là màn hình chuyển tới (.class)
+        thuKichHoatMH2 = new Intent(MainActivity.this,  // Màn hình hiện tại
+                                        MainActivity2.class);         // Màn hình chuyển tới
+
+        // 2. Gửi thư, mà không đợi phản hồi
+        startActivity(thuKichHoatMH2);
     }
 }

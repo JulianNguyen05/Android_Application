29d183d00936f3aa499cce9a0d83e806c970dae2
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-14 22:51:51 +0700

    B1, gửi từ MH1 sang MH2

--- TH_ChuyenMH/app/src/main/java/th/nguyenhuutrong/th_chuyenmh/MainActivity2.java
+++ TH_ChuyenMH/app/src/main/java/th/nguyenhuutrong/th_chuyenmh/MainActivity2.java
@@ -1,6 +1,9 @@
 package th.nguyenhuutrong.th_chuyenmh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,30 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Xây dựng bộ lắng nghe và xử lý theo cách khác
+        // Chú ý: ta có thể làm theo cách giống màn hình 1
+
+        // Cách khác ở đây
+        // 1. Tìm đối tượng cần gắn bộ lắng nghe
+
+        Button btnManHinh2 = findViewById(R.id.btnMH2);
+
+        // 2. Gắn bộ lắng nghe
+
+        btnManHinh2.setOnClickListener(BoLangNgheChuyenMH); // Ta sẽ code ngay dưới
     }
+
+    // Đây nhé, ngoài hàm onCreate
+
+    View.OnClickListener BoLangNgheChuyenMH = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            // Xử lý
+            // 1. Tạo thư
+            Intent thuKichHoatMH1 = new Intent(MainActivity2.this, MainActivity.class);
+            // 2. Gửi đi
+            startActivity(thuKichHoatMH1);
+        }
+    };
 }

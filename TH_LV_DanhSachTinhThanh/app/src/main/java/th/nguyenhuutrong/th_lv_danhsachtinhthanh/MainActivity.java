65b83502de51480f6d21f18e1438a1324a9efb3b
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-15 14:15:13 +0700

    update MainActivity

--- TH_LV_DanhSachTinhThanh/app/src/main/java/th/nguyenhuutrong/th_lv_danhsachtinhthanh/MainActivity.java
+++ TH_LV_DanhSachTinhThanh/app/src/main/java/th/nguyenhuutrong/th_lv_danhsachtinhthanh/MainActivity.java
@@ -1,12 +1,16 @@
 package th.nguyenhuutrong.th_lv_danhsachtinhthanh;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Hiển thị dữ liệu lên ListView
+        // B1+: Cần có dữ liệu
+        //  ?? từ đâu có: từ cơ sở dữ liệu (SQL, noSQL, XML,..)
+        // ở bài này, chúng ta hard-code dữ liệu trực tiếp
+        // Cần biến phù hợp để chứa dữ liệu
+        ArrayList<String> dsTenTT;      // Khai báo
+        dsTenTT = new ArrayList<String>();    // Khởi tạo
+
+            // Thêm dữ liệu ở đây (đúng ra, ta phải đọc từ 1 nguồn)
+            // Nhưng ta hard-code (cho sẵn để demo)
+        dsTenTT.add("Hà Nội");
+        dsTenTT.add("Thành phố Hồ Chí Minh");
+        dsTenTT.add("Đồng Nai");
+        dsTenTT.add("Bình THuận");
+        dsTenTT.add("Ninh Thuận");
+        dsTenTT.add("Nha Trang");
+
+        // B2. Tạo adapter
+        ArrayAdapter<String> adapterTT = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dsTenTT);
+
+        // B3. Gắn vào điều khiển hiển thị ListView
+        // 3.1 Tìm
+        ListView lvTenTT = findViewById(R.id.lvDanhSachTT);
+        // 3.2 Gắn
+        lvTenTT.setAdapter(adapterTT);
+        // 3.3 Lắng nghe và xử lý sự kiện user tương tác
+        // để sau
+
+
     }
 }

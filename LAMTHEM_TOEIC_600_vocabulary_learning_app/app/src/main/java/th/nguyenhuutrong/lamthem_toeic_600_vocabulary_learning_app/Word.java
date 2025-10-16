603c658d6eb8fb9f203a51d268bf617f534a0521
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-16 19:25:48 +0700

    add Topic

--- LAMTHEM_TOEIC_600_vocabulary_learning_app/app/src/main/java/th/nguyenhuutrong/lamthem_toeic_600_vocabulary_learning_app/Word.java
+++ LAMTHEM_TOEIC_600_vocabulary_learning_app/app/src/main/java/th/nguyenhuutrong/lamthem_toeic_600_vocabulary_learning_app/Word.java
@@ -1,34 +1,19 @@
 package th.nguyenhuutrong.lamthem_toeic_600_vocabulary_learning_app;
 
-public class Word {
-    private String word, meaning, example, type;
-    private int imageRes;
+import java.io.Serializable;
 
-    public Word(String word, String meaning, String example, String type, int imageRes) {
+public class Word implements Serializable {
+    private String word, meaning, example, type;
+
+    public Word(String word, String meaning, String example, String type) {
         this.word = word;
         this.meaning = meaning;
         this.example = example;
         this.type = type;
-        this.imageRes = imageRes;
     }
 
-    public String getWord() {
-        return word;
-    }
-
-    public String getMeaning() {
-        return meaning;
-    }
-
-    public String getExample() {
-        return example;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public int getImageRes() {
-        return imageRes;
-    }
+    public String getWord() { return word; }
+    public String getMeaning() { return meaning; }
+    public String getExample() { return example; }
+    public String getType() { return type; }
 }

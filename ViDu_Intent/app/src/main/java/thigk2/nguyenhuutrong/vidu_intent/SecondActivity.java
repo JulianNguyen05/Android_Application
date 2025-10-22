48d20b8f84c25b5395bb28a2867dd33646d0abf1
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-22 09:50:22 +0700

    update activity

--- ViDu_Intent/app/src/main/java/thigk2/nguyenhuutrong/vidu_intent/SecondActivity.java
+++ ViDu_Intent/app/src/main/java/thigk2/nguyenhuutrong/vidu_intent/SecondActivity.java
@@ -1,6 +1,9 @@
 package thigk2.nguyenhuutrong.vidu_intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,23 @@ public class SecondActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second);
 
+        //1. Nhận về Inrent
+        Intent iNhanDuoc = getIntent();
+        //2. Bóc ra
+        String noiDung = iNhanDuoc.getStringExtra("dulieu");
+        String banQuyen = iNhanDuoc.getStringExtra("copyright");
+        //3. Xử lý
+            // Set lên textView
+
+        // Nút back
+        Button btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iQuayVe = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(iQuayVe);
+            }
+        });
 
     }
 }

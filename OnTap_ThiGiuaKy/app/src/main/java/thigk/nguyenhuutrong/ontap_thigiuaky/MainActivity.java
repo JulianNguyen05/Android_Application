3fee3b492cf622049e13221a0b2b2b46ddd5ca1b
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-28 00:01:49 +0700

    update chucNang2

--- OnTap_ThiGiuaKy/app/src/main/java/thigk/nguyenhuutrong/ontap_thigiuaky/MainActivity.java
+++ OnTap_ThiGiuaKy/app/src/main/java/thigk/nguyenhuutrong/ontap_thigiuaky/MainActivity.java
@@ -19,13 +19,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnCN2 = findViewById(R.id.btnCN2);
-        btnCN3 = findViewById(R.id.btnCN3);
-        btnCN4 = findViewById(R.id.btnCN4);
-        btnAbout = findViewById(R.id.btnAbout);
-        btnThem = findViewById(R.id.btnThem);
+//        btnCN2 = findViewById(R.id.btnCN2);
+//        btnCN3 = findViewById(R.id.btnCN3);
+//        btnCN4 = findViewById(R.id.btnCN4);
+//        btnAbout = findViewById(R.id.btnAbout);
+//        btnThem = findViewById(R.id.btnThem);
 
-        btnCN2.setOnClickListener(v -> startActivity(new Intent(this, ActivityChucNang2.class)));
+//        btnCN2.setOnClickListener(v -> startActivity(new Intent(this, ActivityChucNang2.class)));
 //        btnCN3.setOnClickListener(v -> startActivity(new Intent(this, ActivityChucNang3.class)));
 //        btnCN4.setOnClickListener(v -> startActivity(new Intent(this, ActivityChucNang4.class)));
 //        btnAbout.setOnClickListener(v -> startActivity(new Intent(this, ActivityAboutMe.class)));

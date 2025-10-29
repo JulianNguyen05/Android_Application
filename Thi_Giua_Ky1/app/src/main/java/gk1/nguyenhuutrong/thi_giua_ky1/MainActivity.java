a40d51b53558a740ab0dec6a35b4dd2b3b03f4c8
Julian Nguyen <huutrong.160705@gmail.com>
2025-10-29 08:40:44 +0700

    add lam them

--- Thi_Giua_Ky1/app/src/main/java/gk1/nguyenhuutrong/thi_giua_ky1/MainActivity.java
+++ Thi_Giua_Ky1/app/src/main/java/gk1/nguyenhuutrong/thi_giua_ky1/MainActivity.java
@@ -39,9 +39,9 @@ public class MainActivity extends AppCompatActivity {
         btnAbout.setOnClickListener(v ->
                 startActivity(new Intent(MainActivity.this, ActivityGioiThieu.class))
         );
-//
-//        btnThem.setOnClickListener(v ->
-//                startActivity(new Intent(MainActivity.this, AddPlayers.class))
-//        );
+
+        btnThem.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, AddPlayers.class))
+        );
     }
 }

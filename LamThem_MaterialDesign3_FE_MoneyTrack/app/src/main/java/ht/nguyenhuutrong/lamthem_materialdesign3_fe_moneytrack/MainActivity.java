1d26550ca9fb8cc99079698bffc283ef43faf439
Julian Nguyen <huutrong.160705@gmail.com>
2025-11-16 19:45:34 +0700

    update fragment_home.xml

--- LamThem_MaterialDesign3_FE_MoneyTrack/app/src/main/java/ht/nguyenhuutrong/lamthem_materialdesign3_fe_moneytrack/MainActivity.java
+++ LamThem_MaterialDesign3_FE_MoneyTrack/app/src/main/java/ht/nguyenhuutrong/lamthem_materialdesign3_fe_moneytrack/MainActivity.java
@@ -2,23 +2,52 @@ package ht.nguyenhuutrong.lamthem_materialdesign3_fe_moneytrack;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.fragment_home);
+        setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Mặc định mở HomeFragment
+        replaceFragment(new HomeFragment());
+
+        // Bắt sự kiện chọn bottom nav
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment fragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_home) {
+                fragment = new HomeFragment();
+            } else if (id == R.id.nav_transactions) {
+                fragment = new TransactionsFragment();
+            } else if (id == R.id.nav_tools) {
+                fragment = new ToolsFragment();
+            } else if (id == R.id.nav_settings) {
+                fragment = new SettingsFragment();
+            }
+
+            if (fragment != null) {
+                replaceFragment(fragment);
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.fragment_container, new HomeFragment())
+                .replace(R.id.fragment_container, fragment)
                 .commit();
     }
 }

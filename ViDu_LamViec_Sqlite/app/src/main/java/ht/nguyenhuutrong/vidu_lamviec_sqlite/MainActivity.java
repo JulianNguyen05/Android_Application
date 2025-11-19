9f41be037a1e02d07bafcced95d3038db2474778
Julian Nguyen <huutrong.160705@gmail.com>
2025-11-19 09:16:13 +0700

    first

--- ViDu_LamViec_Sqlite/app/src/main/java/ht/nguyenhuutrong/vidu_lamviec_sqlite/MainActivity.java
+++ ViDu_LamViec_Sqlite/app/src/main/java/ht/nguyenhuutrong/vidu_lamviec_sqlite/MainActivity.java
@@ -1,7 +1,10 @@
 package ht.nguyenhuutrong.vidu_lamviec_sqlite;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.sql.SQLData;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,10 +43,36 @@ public class MainActivity extends AppCompatActivity {
 //        db.execSQL(sqlThem3);
 
         // Truy vấn
+        String sqlSelectAll = "SELECT * FROM BOOKS";
+        Cursor resultSet = db.rawQuery(sqlSelectAll, null);
+        ArrayList<String> dsTenSach = new ArrayList<>();
+        resultSet.moveToFirst();
+        while(true){
+            // Lấy dữ liệu
+            int maSach = resultSet.getInt(0);
+            String tenSach =  resultSet.getString(1);
+            int soTrang = resultSet.getInt(2);
+            float giaBan = resultSet.getFloat(3);
+            String moTa =   resultSet.getString(4);
+
+            // Dùng 1 danh sách
+            // Dùng 1 arrayList<String> để chứa tên sách
+            dsTenSach.add(tenSach);
+            // Di chuyen den ban ghi tiep
+            if(resultSet.moveToNext() == false) break;
+        }
+
+
+
 
         // Test
         db.close();
 
+        // Hiện len lv
+        ListView listView = findViewById(R.id.lvDanhSachTenSach);
+        ArrayAdapter<String> adapterTenSach = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, dsTenSach);
+
+        listView.setAdapter(adapterTenSach);
 
 
     }
